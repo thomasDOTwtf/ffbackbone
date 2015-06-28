7b08de7b60298af274e27d53b5a26c314b51e314
thomasDOTwtf <thomas@drewermann.org>
2015-06-28 23:55:02 +0100

    initial commit

---
+++ app/db.py
@@ -0,0 +1,132 @@
+from app import app
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Kennwort1@localhost/testdb'
+db = SQLAlchemy(app)
+
+CommunityCEs = db.Table('CommunityCEs',
+    db.Column('community_id', db.Integer, db.ForeignKey('community.id')),
+    db.Column('CustomerEdge_id', db.Integer, db.ForeignKey('customer_edge.id'))
+)
+
+CommunityASNs = db.Table('CommunityASs',
+    db.Column('community_id', db.Integer, db.ForeignKey('community.id')),
+    db.Column('AS_id', db.Integer, db.ForeignKey('AS.id'))
+)
+
+ASContacts = db.Table('ASContacts',
+    db.Column('as_id', db.Integer, db.ForeignKey('AS.id')),
+    db.Column('contact_id', db.Integer, db.ForeignKey('contact.id'))
+)
+
+CommunityContacts = db.Table('CommunityContacts',
+    db.Column('community_id',db.Integer, db.ForeignKey('community.id')),
+    db.Column('contact_id', db.Integer, db.ForeignKey('contact.id'))
+)
+
+class Community(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(260), unique=True)
+    short = db.Column(db.String(6), unique=True)
+    contacts = db.relationship('Contact', secondary=CommunityContacts,backref=db.backref('Community', lazy='dynamic'))
+    ces = db.relationship('CustomerEdge', secondary=CommunityCEs,backref=db.backref('Community', lazy='dynamic'))
+    asns = db.relationship('AS', secondary=CommunityASNs,backref=db.backref('Community', lazy='dynamic'))
+    prefixes = db.relationship('Prefix', backref='Community', lazy='dynamic')
+    def __repr__(self):
+        return self.name
+
+class CustomerEdge(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(260), unique=True)
+    fqdn = db.Column(db.String(260), unique=True)
+    ipv4 = db.Column(db.String(260), unique=True)
+    ipv6 = db.Column(db.String(260), unique=True)
+    sessions = db.relationship('PeeringSession',  backref='CustomerEdge', lazy='dynamic')
+    def __repr__(self):
+        return self.fqdn
+
+
+class ProviderEdge(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(260), unique=True)
+    fqdn = db.Column(db.String(260), unique=True)
+    ipv4 = db.Column(db.String(260), unique=True)
+    ipv6 = db.Column(db.String(260), unique=True)
+    asn_id = db.Column(db.Integer, db.ForeignKey('AS.id'))
+    site_id = db.Column(db.Integer, db.ForeignKey('site.id'))
+    sessions = db.relationship('PeeringSession', backref='ProviderEdge', lazy='dynamic')
+    def __repr__(self):
+        return self.fqdn
+
+
+class AS(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    asn = db.Column(db.Integer, unique=True)
+    name = db.Column(db.String(260), unique=True)
+    descr = db.Column(db.String(260), unique=True)
+    contacts = db.relationship('Contact', secondary=ASContacts,backref=db.backref('AS', lazy='dynamic'))
+    provideredges = db.relationship('ProviderEdge', backref='AS', lazy='dynamic', uselist='False')
+    def __repr__(self):
+        return 'AS{self.asn}'.format(self=self)
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mail = db.Column(db.String(260), unique=True)
+    nickname = db.Column(db.String(260), unique=True)
+    xmpp = db.Column(db.String(260), unique=True)
+    firstname = db.Column(db.String(260), unique=True)
+    lastname = db.Column(db.String(260), unique=True)
+    login = db.Column(db.String(260), unique=True)
+    password = db.Column(db.String(260), unique=True)
+    handle = db.Column(db.String(260), unique=True)
+    def __repr__(self):
+        return '{self.nickname} ({self.mail})'.format(self=self)
+
+class Site(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(260), unique=True)
+    provideredges = db.relationship('ProviderEdge', backref='Site', lazy='dynamic')
+    prefixes = db.relationship('Prefix', backref='Site', lazy='dynamic')
+    def __repr__(self):
+        return self.name
+
+
+class PeeringSession(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pe_id = db.Column(db.Integer, db.ForeignKey('provider_edge.id'))
+    ce_id = db.Column(db.Integer, db.ForeignKey('customer_edge.id'))
+    pe_v4 = db.Column(db.String(260), unique=True)
+    pe_v6 = db.Column(db.String(260), unique=True)
+    ce_v4 = db.Column(db.String(260), unique=True)
+    ce_v6 = db.Column(db.String(260), unique=True)
+    sessiontype_id = db.Column(db.Integer, db.ForeignKey('session_type.id'))
+    def __repr__(self):
+        return '{self.pe_v4} - {self.ce_ipv4}'.format(self=self)
+
+
+class Prefix(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prefix = db.Column(db.String(260), unique=True)
+    version = db.Column(db.Integer)
+    community_id = db.Column(db.Integer, db.ForeignKey('community.id'))
+    nameserer_id = db.Column(db.Integer, db.ForeignKey('name_server.id'))
+    site_id = db.Column(db.Integer, db.ForeignKey('site.id'))
+    def __repr__(self):
+        return self.prefix
+
+    
+class NameServer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fqdn = db.Column(db.String(260), unique=True)
+    community_id = db.Column(db.Integer, db.ForeignKey('community.id'))
+    prefixes = db.relationship('Prefix', backref='NameServer', lazy='dynamic')
+    def __repr__(self):
+        return self.fqdn
+
+class SessionType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    def __repr__(self):
+        return self.name
+
+db.create_all()
---
+++ app/views.py
@@ -0,0 +1,23 @@
+from app import app
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from db import *
+
+admin = Admin(app)
+admin.add_view(ModelView(Community, db.session))
+admin.add_view(ModelView(ProviderEdge, db.session))
+admin.add_view(ModelView(CustomerEdge, db.session))
+admin.add_view(ModelView(Site, db.session))
+admin.add_view(ModelView(Prefix, db.session))
+admin.add_view(ModelView(AS, db.session))
+admin.add_view(ModelView(Contact, db.session))
+admin.add_view(ModelView(PeeringSession, db.session))
+admin.add_view(ModelView(NameServer, db.session))
+admin.add_view(ModelView(SessionType, db.session))
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html', title='Home', user=user)
